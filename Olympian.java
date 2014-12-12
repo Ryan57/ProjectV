3eaec6c14e14ab974b68f2b865d3d8c3c62477bf
Ryan57 <Ryan.Owens1@marist.edu>
2014-12-12 14:03:38 -0800

    Create Olympian.java

---
+++ Olympian.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+public class Olympian {
+
+    String name;
+    Sex sex;
+    int age;
+
+    Olympian(String name, Sex sex, int age){
+
+        this.name = name;
+        this.sex  = sex;
+        this.age  = age;
+    }//..
+
+}/* End of Olympian Class */
