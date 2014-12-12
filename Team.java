dc152c282f881a8b13bf4e34b57213f1085d7c1d
Ryan57 <Ryan.Owens1@marist.edu>
2014-12-12 14:05:54 -0800

    Create Team.java

---
+++ Team.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Team {
+
+    Olympian olympian1;
+    Olympian olympian2;
+
+        int wins,losses; /* Records each teams values of victories and loses */
+
+
+    Team(Olympian olympian1, Olympian olympian2) {
+
+        this.olympian1 = olympian1;
+        this.olympian2 = olympian2;
+
+    }/* Creates a team comprised of two olympians */
+
+}/* End Class Team */
